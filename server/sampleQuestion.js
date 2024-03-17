184a18e63f74c9e1feadfc4ff53f0e360500907b
swastik2207 <chatterjee.swastik022@gmail.com>
2024-03-17 15:39:26 +0530

    Sample Question Updated

--- server/sampleQuestion.js
+++ server/sampleQuestion.js
@@ -2,6 +2,7 @@ let sampleQuestions = [
   {
     description:
       "Describe the concept of object-oriented programming (OOP) and explain its advantages.",
+    subject:["OOP"],
     chapterName: ["Programming Paradigms", "Object-Oriented Programming"],
     topicTag: ["OOP", "Advantages"],
     ans: "Object-oriented programming (OOP) is a programming paradigm based on the concept of objects, which can contain data and code. It allows for encapsulation, inheritance, and polymorphism, leading to better organization, reusability, and maintainability of code.",
@@ -14,6 +15,7 @@ let sampleQuestions = [
   {
     description:
       "What is a binary search tree (BST)? Explain its operations and complexity.",
+      subject:["DSA"],
     chapterName: ["Data Structures", "Trees"],
     topicTag: ["Binary Search Tree", "Operations", "Complexity"],
 
@@ -27,6 +29,7 @@ let sampleQuestions = [
 
   {
     description: "Discuss the difference between TCP and UDP protocols.",
+    subject:["Computer_Networks"],
     chapterName: ["Computer Networks", "Protocols"],
     topicTag: ["TCP", "UDP", "Protocols"],
     ans: "Transmission Control Protocol (TCP) and User Datagram Protocol (UDP) are both transport layer protocols used for communication over networks. TCP provides reliable, connection-oriented communication with error-checking and retransmission mechanisms, while UDP provides unreliable, connectionless communication without error-checking or retransmission.",
@@ -40,6 +43,7 @@ let sampleQuestions = [
   {
     description:
       "Explain the working principle of Dijkstra's algorithm with an example.",
+      subject:["Data_Structures_And_Algorithm"],
     chapterName: ["Algorithms", "Graph Algorithms"],
     topicTag: ["Dijkstra's Algorithm", "Shortest Path"],
     ans: "Dijkstra's algorithm is a greedy algorithm used to find the shortest path between nodes in a graph. It maintains a set of vertices whose shortest distance from the source node is already known. At each step, it selects the vertex with the smallest known distance and updates the distances of its adjacent vertices. This process continues until all vertices have been visited.",
@@ -53,6 +57,7 @@ let sampleQuestions = [
   {
     description:
       "What is the role of a compiler in programming?",
+    subject:["Compiler_Design"],
     chapterName: ["Compiler Design", "Programming Languages"],
     topicTag: ["Compiler", "Compilation Process"],
     ans: "A compiler is a software tool that translates high-level programming language code into machine code or intermediate code. The compilation process consists of several phases, including lexical analysis, syntax analysis, semantic analysis, code generation, and optimization. Lexical analysis involves tokenizing the input code, syntax analysis checks the syntax of the code for correctness, semantic analysis checks the meaning of the code, code generation produces machine code, and optimization improves the efficiency of the generated code.",
@@ -66,6 +71,7 @@ let sampleQuestions = [
   {
     description:
       "Discuss the importance of data structures in computer science and provide examples of commonly used data structures.",
+    subject:["Data_Structures_And_Algorithm","Computer_Networks"],
     chapterName: ["Data Structures", "Computer Science Fundamentals"],
     topicTag: ["Data Structures", "Importance", "Examples"],
     ans: "Data structures are fundamental concepts in computer science that allow for efficient organization, storage, and manipulation of data. Commonly used data structures include arrays, linked lists, stacks, queues, trees, graphs, and hash tables. Each data structure has its own advantages and use cases, and understanding them is essential for designing efficient algorithms and programs.",
@@ -79,6 +85,8 @@ let sampleQuestions = [
   {
     description:
       "Describe the concept of inheritance in object-oriented programming and provide an example.",
+
+      subject:["Object_Oriented_Programmming"],
     chapterName: ["Programming Paradigms", "Object-Oriented Programming"],
     topicTag: ["Inheritance", "OOP"],
     ans: "Inheritance is a key concept in object-oriented programming (OOP) that allows a class (subclass) to inherit properties and methods from another class (superclass). This enables code reuse and promotes the creation of hierarchical relationships between classes. For example, a 'Vehicle' class can be a superclass with properties like 'color' and 'speed', and subclasses like 'Car' and 'Bike' can inherit these properties while adding their own specific features.",
@@ -92,6 +100,7 @@ let sampleQuestions = [
   {
     description:
       "What is the purpose of an operating system? Explain the functions of an operating system.",
+      subject:["Operating_System"],
     chapterName: ["Operating Systems", "Computer Science Fundamentals"],
     topicTag: ["Operating System", "Functions"],
     ans: "An operating system (OS) is a software system that manages computer hardware and software resources and provides services for computer programs. The functions of an operating system include process management, memory management, file system management, device management, security, and user interface. It acts as an intermediary between users and hardware, allowing users to interact with the computer system efficiently.",
@@ -105,6 +114,8 @@ let sampleQuestions = [
   {
     description:
       "Discuss the concept of virtual memory and explain its benefits in computer systems.",
+   
+      subject:["System_Design"],
     chapterName: ["Operating Systems", "Memory Management"],
     topicTag: ["Virtual Memory", "Benefits"],
     ans: "Virtual memory is a memory management technique that uses disk space to simulate additional RAM (random-access memory) when the physical memory (RAM) is full. It allows programs to use more memory than is physically available, enabling multitasking and efficient memory utilization. Virtual memory provides benefits such as increased system performance, better system reliability, and improved memory management.",
@@ -118,6 +129,7 @@ let sampleQuestions = [
   {
     description:
       "Explain the difference between synchronous and asynchronous programming paradigms, and provide examples of their usage.",
+     subject:["Object_Oriented_Design"],
     chapterName: ["Programming Paradigms", "Concurrency"],
     topicTag: ["Synchronous", "Asynchronous", "Concurrency"],
     ans: "Synchronous programming is a programming paradigm in which operations are performed one after another, blocking the execution until each operation completes. Asynchronous programming, on the other hand, allows operations to be performed concurrently, without blocking the execution. Asynchronous programming is commonly used in scenarios such as I/O operations, network communication, and event-driven programming, where it improves responsiveness and efficiency by allowing non-blocking execution of tasks.",
